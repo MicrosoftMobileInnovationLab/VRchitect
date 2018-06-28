92eb875697055cc9d4546210dc4de7239b782abf
baddwolff <40377333+baddwolff@users.noreply.github.com>
2018-06-28 08:35:54 +0530

    slight mods

--- Vrchitect_kivy/main.py
+++ Vrchitect_kivy/main.py
@@ -6,16 +6,26 @@ from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-
+import cv2
+import numpy as np
 import os
 
+str_path=""
+str_name=""
+h=0.0
+l=0.0
+b=0.0
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
+
 class Root(FloatLayout):
 
+    hgt = ObjectProperty(None)
+    len = ObjectProperty(None)
+    bre = ObjectProperty(None)
     loadfile = ObjectProperty(None)
     #text_input = ObjectProperty(None)
 
@@ -30,10 +40,17 @@ class Root(FloatLayout):
 
     def load(self, path, filename):
         #file_path=os.path.join(path,filename[0])
-        print(path)
-        #  with open(os.path.join(path, filename[0])) as stream:
-        #    self.text_input.text = stream.read()
+
+        str_path=str(filename[0])
+        print(str_path)
+        img=cv2.imread(str_path)
         self.dismiss_popup()
+
+    def sub(self):
+        h=float(self.hgt.text)
+        b=float(self.bre.text)
+        l=float(self.len.text)
+        print(h,b,l)
 
 class Editor(App):
     pass
@@ -41,8 +58,6 @@ class Editor(App):
 
 Factory.register('Root', cls=Root)
 Factory.register('LoadDialog', cls=LoadDialog)
-
-
 
 if __name__ == '__main__':
     Editor().run()
